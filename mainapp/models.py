a467a28c7124bb6030f14208c21318692c1e731b
j0hanpohan <y_shakal@mail.ru>
2023-01-30 17:05:55 +0300

    lesson_6 Customizing admin panel

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class News(models.Model):
@@ -17,11 +18,16 @@ class News(models.Model):
         self.deleted = True
         self.save()
 
+    class Meta:
+        verbose_name = _("News")
+        verbose_name_plural = _("News")
+        ordering = ("-created",)
+
 
 class CoursesManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(deleted=False)
-        
+
 
 class Courses(models.Model):
     objects = CoursesManager()
@@ -62,6 +68,8 @@ class Lesson(models.Model):
 
     class Meta:
         ordering = ("course", "num")
+        verbose_name = _("Lesson")
+        verbose_name_plural = _("Lessons")
 
 
 class CourseTeachers(models.Model):
@@ -77,3 +85,7 @@ class CourseTeachers(models.Model):
     def delete(self, *args):
         self.deleted = True
         self.save()
+
+    class Meta:
+        verbose_name = _("Teacher")
+        verbose_name_plural = _("Teachers")
